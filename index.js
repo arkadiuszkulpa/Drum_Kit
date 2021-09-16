865257c5b6026291dbfed93cb7e8a3f2fb721a55
Arkadiusz Kulpa <arkadiusz.kulpa.91@gmail.com>
2021-09-16 12:17:57 +0100

    buttonpress works

--- index.js
+++ index.js
@@ -6,53 +6,55 @@
 
 var buttons = document.querySelectorAll(".drum");
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", handleClick);
+  buttons[i].addEventListener("click", function() {
+    makeSound(this.innerHTML);
+  });
+
 }
 
-function handleClick() {
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+});
 
-  var buttoninnerHTML = this.innerHTML;
-
-  switch (buttoninnerHTML) {
+function makeSound(key) {
+  switch (key) {
     case "w":
-    var tom1 = new Audio('sounds/tom-1.mp3');
-    tom1.play();
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
       break;
 
     case "a":
-    var tom2 = new Audio('sounds/tom-2.mp3');
-    tom2.play();
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
       break;
 
     case "s":
-    var tom3 = new Audio('sounds/tom-3.mp3');
-    tom3.play();
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
       break;
 
     case "d":
-    var tom4 = new Audio('sounds/tom-4.mp3');
-    tom4.play();
-    break;
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
 
     case "j":
-    var snare = new Audio('sounds/snare.mp3');
-    snare.play();
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
       break;
 
     case "k":
-    var crash = new Audio('sounds/crash.mp3');
-    crash.play();
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
       break;
 
     case "l":
-    var kick = new Audio('sounds/basic-drum-kick.wav');
-    kick.play();
+      var kick = new Audio('sounds/basic-drum-kick.wav');
+      kick.play();
       break;
 
 
-    default: console.log(buttoninnerHTML);
-
-
+    default:
+      console.log(key);
   }
-
 }
